7a38d371f4ce3ad337800e1060467b583073b8d0
4Kamei <aleksander.kaminski@protonmail.com>
2021-09-04 21:15:59 +0100

    decoding of mac and ethertype

--- src_test/ethernet_rx/ethernet_rx.py
+++ src_test/ethernet_rx/ethernet_rx.py
@@ -1,8 +1,11 @@
 import cocotb
 import logging
+from scapy.all import Ether, ARP
 from cocotb.clock import Clock
-from cocotb.triggers import Event, Timer
+from cocotb.triggers import Event, ClockCycles
 from cocotbext.eth import MiiSource, GmiiFrame, MiiPhy
+
+IP_ADDR = "192.168.100.0"
 
 @cocotb.test()
 async def test_ethernet_rx(dut):
@@ -13,24 +16,34 @@ async def test_ethernet_rx(dut):
     l = logging.getLogger("cocotb")
 
     l.setLevel(logging.DEBUG)
+        
+    dut.reset <= 1;
 
-    l.info("testing mii data")
+    await ClockCycles(dut.rx_clk, 10)
 
-    mii_src = MiiSource(dut.rxd, None, dut.rx_dv, dut.rx_clk, dut.reset)
+    dut.reset <= 0;
 
-    #TODO WRITE SCAPY PACKETS
 
-    frame = GmiiFrame.from_payload(b'hello world', tx_complete=Event())
+    packet = Ether(dst="01:23:45:67:89:ab")/ARP(pdst=IP_ADDR)
     
-    l.info("sending data")
-    
+    bts = bytearray(packet.build())
+
+    mii_src = MiiSource(dut.rxd, None, dut.rx_dv, dut.rx_clk)
+
+    frame = GmiiFrame.from_payload(bts)
+
     await mii_src.send(frame)
 
-    l.info("waiting for data to send")
+    packet = Ether(dst="ff:ff:ff:ff:ff:00")/ARP(pdst=IP_ADDR)
     
+    frame2 = GmiiFrame.from_payload(bytearray(packet.build()), tx_complete=Event())
+
+    await mii_src.send(frame2)
+        
     await mii_src.wait()
-    
-    l.info("finished sending")
+
+    await frame2.tx_complete.wait()
+
     assert True
     
 
