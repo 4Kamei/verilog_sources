7c595c04914bb600b50b5527de7cba0ba464a1a6
4Kamei <aleksander.kaminski@protonmail.com>
2021-09-04 21:15:59 +0100

    added translations for state enum for use with GTKWAVE

--- src/ethernet_rx.sv
+++ src/ethernet_rx.sv
@@ -17,35 +17,60 @@ module ethernet_rx (
         input wire i_ethernet
     );
 
-    typedef enum [1:0] {IDLE, SYNC, REC_HEADER, REC_PAYLOAD} ethernet_state /*verilator public*/;
+    typedef enum [2:0] {IDLE, SYNC, REC_DSTMAC, REC_SRCMAC, REC_ETHERTYPE, PAYLOAD} ethernet_state /*verilator public*/;
 
-    typedef enum {IP, UNK} ethernet_type /*verilator public*/;
+    typedef enum [1:0] {IP, UNK} ethernet_type /*verilator public*/;
 
     reg [1:0] bits;
     reg has_edge;
 
+
     ethernet_state state;
 
+    reg [15:0] bitCounter;
+    reg [47:0] dst_mac_brev;
+    reg [47:0] src_mac_brev;
 
-    reg [6:0] bitCounter;
     /* verilator lint_off UNUSED */
-    reg [111:0] header; //MAC DST, MAC SRC, ETHERTYPE
-    /* verilator lint_on UNUSED */
+    
+    reg [15:0] ethertype_brev;
+    
+    ethernet_type ethertype;
 
+    wire [47:0] dst_mac;
+    wire [47:0] src_mac;
+
+    //reverse the order of the bytes for both destination and source mac
+
+    assign dst_mac = {dst_mac_brev[ 7:0], 
+                      dst_mac_brev[15:8],
+                      dst_mac_brev[23:16],
+                      dst_mac_brev[31:24],
+                      dst_mac_brev[39:32],
+                      dst_mac_brev[47:40]};
+
+    assign src_mac = {src_mac_brev[ 7:0], 
+                      src_mac_brev[15:8],
+                      src_mac_brev[23:16],
+                      src_mac_brev[31:24],
+                      src_mac_brev[39:32],
+                      src_mac_brev[47:40]}; 
+
+    /* verilator lint_on UNUSED */
+    
     always @(posedge i_clkQ) begin
         bits <= {bits[0], i_ethernet}; //shift bits lower
-    end
-
-    always @(negedge i_clkQ) begin
-        if(state == REC_HEADER) begin
-            bitCounter <= bitCounter + 1'b1;
-        end
     end
 
     //triggering on negedge as that's the next edge of the clocks i_clkQ and
     //i_clk. 
     always @(negedge i_clk) begin
-        has_edge <= bits[0] ^ bits[1];
+        /* verilator lint_off BLKSEQ */
+        has_edge = bits[0] ^ bits[1];
+        /* verilator lint_on BLKSEQ */
+        if(state != IDLE & state != SYNC) begin
+            bitCounter <= bitCounter + 1; //increment bit counter whenever a new bit comes in, 
+        end
         case(state)
             IDLE: begin
                 if(has_edge) begin
@@ -54,18 +79,32 @@ module ethernet_rx (
             end
             SYNC: begin
                 if(!has_edge) begin
-                    state <= REC_HEADER;
+                    state <= REC_DSTMAC;
+                    bitCounter <= 0; 
+
                 end
             end
-            REC_HEADER: begin
-                header[bitCounter] <= bits[0];
-                if(bitCounter == 7'd111) begin
-                    state <= REC_PAYLOAD;
+            REC_DSTMAC: begin
+                dst_mac_brev[bitCounter[5:0]] <= bits[0];
+                if(bitCounter == 16'd47) begin
+                    state <= REC_SRCMAC;
+                    bitCounter <= 0;
                 end
             end
-            REC_PAYLOAD: begin
-                state <= IDLE;
+            REC_SRCMAC: begin
+                src_mac_brev[bitCounter[5:0]] <= bits[0];
+                if(bitCounter == 16'd47) begin
+                    state <= REC_ETHERTYPE;
+                    bitCounter <= 0;
+                end
             end
+            REC_ETHERTYPE: begin
+                ethertype_brev[bitCounter[3:0]] <= bits[0];
+                if(bitCounter == 16'd15) begin
+                    state <= PAYLOAD;
+                end
+            end
+            default: assert(0);
         endcase
     end
 
