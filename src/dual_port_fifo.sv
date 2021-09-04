61a30ee6b485ef9e8ba10400a52c57f15e46ea2e
4Kamei <aleksander.kaminski@protonmail.com>
2021-09-04 21:15:59 +0100

    dual port fifo tested and working

--- src/dual_port_fifo.sv
+++ src/dual_port_fifo.sv
@@ -9,10 +9,10 @@ module dual_port_fifo
         input wire wr_clk,
         input wire wr_en,
         input wire [DATA_WIDTH - 1: 0] wr_data,
-        output wire full,
+        output reg full,
 
         input wire rd_clk,
-        output wire rd_en,
+        input wire rd_en,
         output reg [DATA_WIDTH - 1: 0] rd_data,
         output wire empty,
 
@@ -30,58 +30,62 @@ module dual_port_fifo
 
     logic [DATA_WIDTH - 1: 0] fifo_data [FIFO_DEPTH - 1: 0];
 
-    logic [DATA_WIDTH - 1: 0] read_head;
-
-
     //READ CLOCK DOMAIN
-    logic [POINTER_WIDTH-1:0] rd_pointer_binary;
-    logic [POINTER_WIDTH-1:0] rd_pointer_gray;   
-    logic [POINTER_WIDTH-1:0] rd_pointer_gray_incremented;   
-    logic [POINTER_WIDTH-1:0] rdcdc_wr_pointer_gray;
+    logic [POINTER_WIDTH:0] rd_pointer_binary;
+    logic [POINTER_WIDTH:0] rd_pointer_gray;   
+    logic [POINTER_WIDTH:0] rd_pointer_gray_incremented;   
+    logic [POINTER_WIDTH:0] rdcdc_wr_pointer_gray;
 
-    gray2bin #(.BINARY_WIDTH(POINTER_WIDTH))
+    gray2bin #(.BINARY_WIDTH(POINTER_WIDTH + 1))
         rd_side_g2b(.gray(rd_pointer_gray), .binary(rd_pointer_binary));
     
-    bin2gray #(.BINARY_WIDTH(POINTER_WIDTH))
+    bin2gray #(.BINARY_WIDTH(POINTER_WIDTH + 1))
         rd_side_b2g(.gray(rd_pointer_gray_incremented), .binary(rd_pointer_binary + 1'b1));
     
+        
     assign empty = rdcdc_wr_pointer_gray == rd_pointer_gray;
 
-    always @(posedge rd_clk, posedge reset) begin
+    always @(*) begin
+        rd_data <= fifo_data[rd_pointer_binary[POINTER_WIDTH-1:0]];
+    end
+
+    always @(posedge rd_clk) begin
         //CDC of wr pointer
         rdcdc_wr_pointer_gray <= wr_pointer_gray;
         if(reset) begin
             rd_pointer_gray <= 0;
         end else begin
             if(rd_en & ~empty) begin
-                rd_data <= fifo_data[rd_pointer_binary];
-                rd_pointer_gray <= rd_pointer_gray_incremented;
+                rd_pointer_gray = rd_pointer_gray_incremented;
             end
         end
     end
 
-
     ///WRITE CLOCK DOMAIN
-    logic [POINTER_WIDTH-1:0] wr_pointer_binary;
-    logic [POINTER_WIDTH-1:0] wr_pointer_gray;   
-    logic [POINTER_WIDTH-1:0] wr_pointer_gray_incremented;   
-    logic [POINTER_WIDTH-1:0] wrcdc_rd_pointer_gray;
+    logic [POINTER_WIDTH:0] wr_pointer_binary;
+    logic [POINTER_WIDTH:0] wr_pointer_gray;   
+    logic [POINTER_WIDTH:0] wr_pointer_gray_incremented;   
+    logic [POINTER_WIDTH:0] wrcdc_rd_pointer_gray;
+    logic [POINTER_WIDTH:0] wrcdc_rd_pointer_binary;
     
-    gray2bin #(.BINARY_WIDTH(POINTER_WIDTH))
+    gray2bin #(.BINARY_WIDTH(POINTER_WIDTH + 1))
         wr_side_g2b(.gray(wr_pointer_gray), .binary(wr_pointer_binary));
     
-    bin2gray #(.BINARY_WIDTH(POINTER_WIDTH))
+    bin2gray #(.BINARY_WIDTH(POINTER_WIDTH + 1))
         wr_side_b2g(.gray(wr_pointer_gray_incremented), .binary(wr_pointer_binary + 1'b1));
 
-    assign full = wrcdc_rd_pointer_gray == wr_pointer_gray_incremented;
+    gray2bin #(.BINARY_WIDTH(POINTER_WIDTH + 1))
+        wr_side_pointer_g2b(.gray(wrcdc_rd_pointer_gray), .binary(wrcdc_rd_pointer_binary));
+    
+    assign full = wrcdc_rd_pointer_binary == {~wr_pointer_binary[POINTER_WIDTH], wr_pointer_binary[POINTER_WIDTH - 1:0]};
 
-    always @(posedge wr_clk, posedge reset) begin
+    always @(posedge wr_clk) begin
         wrcdc_rd_pointer_gray <= rd_pointer_gray;
         if(reset) begin
             wr_pointer_gray <= 0;
         end else begin
             if(wr_en & ~full) begin
-                fifo_data[wr_pointer_binary] <= wr_data;
+                fifo_data[wr_pointer_binary[POINTER_WIDTH-1:0]] <= wr_data;
                 wr_pointer_gray <= wr_pointer_gray_incremented;
             end
         end
--- src_test/fifo/dual_port_fifo.py
+++ src_test/fifo/dual_port_fifo.py
@@ -2,89 +2,147 @@ import logging
 import random
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles, Combine, RisingEdge, Timer, FallingEdge
+from cocotb.triggers import ClockCycles, Combine, RisingEdge, Timer,FallingEdge
+from cocotb.binary import BinaryValue
 
 import queue
 
-q = queue.Queue()
+class TB:
 
-l = logging.getLogger("cocotb")
+    def __init__(self, dut):
+        self.log = logging.getLogger("cocotb")
+        self.dut = dut;
+        self.queue = queue.Queue()
+        self.queue_width = 16
+        self.wr_clk_period = 13
+        self.rd_clk_period = 7
 
-bit_num = 16
+    async def start(self):
+        self.dut.rd_en <= 0
+        self.dut.wr_en <= 0
+        self.dut.reset <= 0
 
-async def push(wr_data, dut):
-    dut.wr_en <= 1
-    dut.wr_data <= wr_data
-    await ClockCycles(dut.wr_clk, 1)
-    dut.wr_en <= 0
+        wc = Clock(self.dut.wr_clk, self.wr_clk_period, 'ns')
+        cocotb.fork(wc.start())
+        
+        rc = Clock(self.dut.rd_clk, self.rd_clk_period, 'ns')
+        cocotb.fork(rc.start())
+
+
+    async def reset(self) -> None:
+        self.dut.reset <= 1
+        
+        await ClockCycles(self.dut.rd_clk, 1)
+        await ClockCycles(self.dut.wr_clk, 1)
+        await ClockCycles(self.dut.rd_clk, 1)
+        await ClockCycles(self.dut.wr_clk, 1)
+        
+        self.dut.reset <= 0
+        
+        await ClockCycles(self.dut.rd_clk, 1)
+        await ClockCycles(self.dut.wr_clk, 1)
     
-    if(dut.full == 0):
-        q.put(wr_data)
-        return True
-    return False
+        
+        
+    def stop_write(self):
+        self.dut.wr_en <= 0
+        self.dut.wr_data <= BinaryValue('z' * self.queue_width)
 
-async def pop(dut):
+    def stop_read(self):
+        self.dut.rd_en <= 0
+
+    async def push(self, wr_data):
+        await FallingEdge(self.dut.wr_clk)
+        
+        if(self.dut.full == 1):
+            return False
+        
+        self.dut.wr_en <= 1
+        self.dut.wr_data <= wr_data
+        await RisingEdge(self.dut.wr_clk)
+        self.queue.put(wr_data)
+        self.log.info("pushing {}".format(wr_data))
+        return True
     
-    await RisingEdge(dut.rd_clk)
+    async def pop(self):  
+        await FallingEdge(self.dut.rd_clk)
+        
+        if(self.dut.empty == 1):
+            self.log.info("empty status == {}".format(self.dut.empty))
+            if(not self.queue.empty()):
+                self.log.info(list(map(hex, self.queue.queue)))
+            return False, None
+        
 
-    #data here invalid?
-    data = dut.rd_data.value.integer
-    if(dut.empty == 0):
-        data2 = q.get()
-        assert data == data2,("data read and data popped don't match {} != {}".format(hex(data), hex(data2)))
-        return True
-    return False
+
+        self.dut.rd_en <= 1
+        data = self.dut.rd_data.value.integer
+        await RisingEdge(self.dut.rd_clk)
+        self.log.info("popping {}".format(data))
+        
+        test_data = self.queue.get()
+        assert data == test_data, "pushed and popped values don't match! read {}, expected {}".format(hex(data), hex(test_data))
+
+        return True, data
 
 @cocotb.test()
-async def test(dut):
+async def write_then_read(device):
+              
+    dut = TB(device)
 
+    await dut.start()
+    await dut.reset()
 
-    dut.rd_en <= 0
-
-    random.seed(248234)
-
-    wc = Clock(dut.wr_clk, 10, 'ns')
-    cocotb.fork(wc.start())
-    
-    rc = Clock(dut.rd_clk, 3, 'ns')
-    cocotb.fork(rc.start())
-    
-
-    dut.reset <= 1
-
-    await Combine(ClockCycles(dut.wr_clk, 2), ClockCycles(dut.rd_clk, 2))
-
-    dut.reset <= 0
-    
-
+    dut.log.info("device reset")
+ 
     #push until full
     while(True):
-        ret = await push(random.getrandbits(bit_num), dut)
-        if(not ret):
+        data = random.getrandbits(dut.queue_width)       
+        result = await dut.push(data)
+        if(not result):
             break
 
-    l.info(list(map(lambda x: hex(x), q.queue)))
+    dut.stop_write()
 
-    l.info("pushed {} elements into queue".format(q.qsize()))
-    
-    await ClockCycles(dut.rd_clk, 1)
+    dut.log.info("queue full, popping all elements")
 
-    l.info("waiting a little bit")
-    
-    dut.rd_en <= 1
-
-    await RisingEdge(dut.rd_clk)
-
+   
     while(True):
-        ret = await pop(dut)
+        ret, data = await dut.pop()
         if(not ret):
             break
+    
+    dut.stop_read()
 
-    dut.rd_en <= 0
 
-    l.info("popped all elements")
+    dut.log.info("popped all elements")
+
+
+@cocotb.test()
+async def write_and_read(device):
+
+    dut = TB(device)
+
+    await dut.start()
+    await dut.reset()
+
+    #dut should be in predictable state
+    
+    dut.log.info("device reset")
+
+    operations = 2000
+    while(operations > 0):
+        operations -= 1
+        read = random.randint(0, 1) == 0
+        if(read):
+            dut.stop_write()
+            result, data = await dut.pop()
+
+        else:
+            dut.stop_read()
+            data = random.getrandbits(dut.queue_width)
+            result = await dut.push(data)
 
     
-
     
     
